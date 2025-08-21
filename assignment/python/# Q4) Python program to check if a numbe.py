59480e28188f3a4f4c7cb71a8dab6ee94f275620
dhruvkothari52 <dhruv52jain@gmail.com>
2025-08-21 12:22:39 +0530

    Add files via upload

---
+++ assignment/python/# Q4) Python program to check if a numbe.py
@@ -0,0 +1,21 @@
+# Q4) Python program to check if a number is positive, negative, or zero
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("The number is Positive")
+elif num < 0:
+    print("The number is Negative")
+else:
+    print("The number is Zero")
+
+
+# Q10) Check even or odd
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+#till Q 10
